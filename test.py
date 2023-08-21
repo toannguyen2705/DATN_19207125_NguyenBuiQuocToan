f342dbc4688a8534961eb63a74adfd82eab07a2b
ToanNguyen2705 <toannguyen.27052001@gmail.com>
2023-08-22 00:20:16 +0700

    update

--- test.py
+++ test.py
@@ -3,19 +3,19 @@ import dlib
 import pyttsx3
 from scipy.spatial import distance
 
-# INITIALIZING pyttsx3 FOR ALERT AUDIO MESSAGES
+# Khởi tạo pyttsx3 để phát ra thông báo âm thanh
 engine = pyttsx3.init()
 
-# SETTING UP THE CAMERA (0 for built-in webcam, 1 for external camera)
+# Thiết lập camera (0 cho webcam tích hợp sẵn, 1 cho camera ngoài)
 cap = cv2.VideoCapture(0)
 
-# FACE DETECTION USING dlib
+# Sử dụng dlib để phát hiện khuôn mặt
 face_detector = dlib.get_frontal_face_detector()
 
-# LOAD THE LANDMARKS MODEL FOR FACE
+# Tải mô hình landmarks cho khuôn mặt
 dlib_facelandmark = dlib.shape_predictor("C:\\Users\\ACER\\Downloads\\shape_predictor_68_face_landmarks.dat")
 
-# FUNCTION TO CALCULATE THE ASPECT RATIO OF THE EYE
+# Hàm để tính tỉ lệ khung hình của mắt
 def calculate_aspect_ratio(eye):
     poi_A = distance.euclidean(eye[1], eye[5])
     poi_B = distance.euclidean(eye[2], eye[4])
@@ -23,8 +23,8 @@ def calculate_aspect_ratio(eye):
     aspect_ratio_eye = (poi_A + poi_B) / (2 * poi_C)
     return aspect_ratio_eye
 
-# MAIN LOOP TO CONTINUOUSLY PROCESS FRAMES
-frame_skip_interval = 5  # Process every 5th frame
+# Cài đặt thời gian chờ giữa các khung hình để giảm tải xử lý
+frame_skip_interval = 5
 frame_count = 0
 
 while True:
@@ -45,46 +45,43 @@ while True:
         left_eye = []
         right_eye = []
 
-        # POINTS ALLOCATION FOR THE LEFT EYE (42 to 47 in the .dat file)
-        for n in range(42, 48):
-            x = face_landmarks.part(n).x
-            y = face_landmarks.part(n).y
-            right_eye.append((x, y))
-            next_point = n + 1 if n < 47 else 42
-            x2 = face_landmarks.part(next_point).x
-            y2 = face_landmarks.part(next_point).y
-            cv2.line(frame, (x, y), (x2, y2), (0, 255, 0), 1)
+        # Xác định điểm cho mắt trái và mắt phải
+        eye_points = [(range(42, 48), (0, 255, 0)), (range(36, 42), (255, 255, 0))]
 
-        # POINTS ALLOCATION FOR THE RIGHT EYE (36 to 41 in the .dat file)
-        for n in range(36, 42):
-            x = face_landmarks.part(n).x
-            y = face_landmarks.part(n).y
-            left_eye.append((x, y))
-            next_point = n + 1 if n < 41 else 36
-            x2 = face_landmarks.part(next_point).x
-            y2 = face_landmarks.part(next_point).y
-            cv2.line(frame, (x, y), (x2, y2), (255, 255, 0), 1)
+        for point_range, color in eye_points:
+            eye = [(face_landmarks.part(n).x, face_landmarks.part(n).y) for n in point_range]
 
-        # CALCULATING THE ASPECT RATIO FOR LEFT AND RIGHT EYES
+            for n in range(len(eye)):
+                x, y = eye[n]
+                next_point = n + 1 if n < len(eye) - 1 else 0
+                x2, y2 = eye[next_point]
+                cv2.line(frame, (x, y), (x2, y2), color, 1)
+
+            if point_range == range(42, 48):
+                right_eye = eye
+            else:
+                left_eye = eye
+
+        # Tính tỉ lệ khung hình cho mắt trái và mắt phải
         right_eye_aspect_ratio = calculate_aspect_ratio(right_eye)
         left_eye_aspect_ratio = calculate_aspect_ratio(left_eye)
         average_eye_aspect_ratio = (left_eye_aspect_ratio + right_eye_aspect_ratio) / 2
 
-        # ROUNDING OFF THE AVERAGE MEAN OF RIGHT AND LEFT EYES
+        # Làm tròn tỉ lệ trung bình của mắt trái và phải
         average_eye_aspect_ratio = round(average_eye_aspect_ratio, 2)
 
-        # DETECTING DROWSINESS BASED ON THE EYE ASPECT RATIO
+        # Phát hiện tình trạng buồn ngủ dựa trên tỉ lệ khung hình của mắt
         if average_eye_aspect_ratio < 0.25:
             cv2.putText(frame, "DROWSINESS DETECTED", (50, 100), cv2.FONT_HERSHEY_PLAIN, 2, (21, 56, 210), 3)
             cv2.putText(frame, "Alert!!!! WAKE UP", (50, 450), cv2.FONT_HERSHEY_PLAIN, 2, (21, 56, 212), 3)
 
-            # CALLING THE TEXT-TO-SPEECH FUNCTION TO ALERT THE PERSON
+            # Gọi hàm chuyển văn bản thành giọng để cảnh báo người dùng
             engine.say("Alert!!!! WAKE UP")
             engine.runAndWait()
 
     cv2.imshow("Drowsiness DETECTOR IN OPENCV2", frame)
     key = cv2.waitKey(1)
-    if key == 27:  # Press 'Esc' to exit
+    if key == 27:  # Nhấn 'Esc' để thoát
         break
 
 cap.release()
